bc4995efc7240c251e838b96d3d61b186973391d
Edan Toledo <edan.toledo@live.com>
2022-12-17 10:54:28 +0200

    uncomment code in training script

--- rllib_code/training_script.py
+++ rllib_code/training_script.py
@@ -18,7 +18,7 @@ from rllib_code.env_wrapper import RLlibEnvWrapper
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.tune.logger import NoopLogger, pretty_print
 
-# ray.init(log_to_driver=False)
+ray.init(log_to_driver=False)
 
 logging.basicConfig(stream=sys.stdout, format="%(asctime)s %(message)s")
 logger = logging.getLogger("main")
