20e23b3184811d8a84180288bcb95f3db48fc46a
Emily Morris <emilymorris@user0924.eduroam.christs.private.cam.ac.uk>
2023-01-16 18:08:18 +0000

    Sensitivty analysis using normalized training data

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -28,12 +28,19 @@ def pass_values(file_name):
     file.close()
     return X, Y
 
-X, Y = pass_values("bo_evaluations.txt")
+X, Y = pass_values("bo_evaluations_3.txt")
+# print("X", X)
+# print("Y", Y)
+# Normalize the data
+Y = Y*(-1)
+Y = Y - Y.mean()
+Y = Y/ Y.std()
 # Format Y to be the corret shape
 Y = Y[:,None]
 
+
 # Treat the marginal tax value of each bracket as a seperate variable
-num_tax_brackets = 50
+num_tax_brackets = 3
 parameters = []
 
 for i in range(num_tax_brackets):
@@ -44,9 +51,11 @@ parameters.append(DiscreteParameter('bracket_cutoffs', [0,1]))
 space = ParameterSpace(parameters)
 
 
-# # For testing stuff with a predefined function
-# X = np.random.randn(30, 51)
-# Y = np.random.randn(30)[:, None]
+# For testing stuff with a predefined function
+# X = np.random.randn(18, 8)
+# Y = np.random.randn(18)[:, None]*9
+# print("X", X)
+# print("Y", Y)
 
 # Do sensitivity analysis using GP surrogate model
 
@@ -67,6 +76,17 @@ model_gpy = GPRegression(X,Y)
 model_emukit = GPyModelWrapper(model_gpy)
 model_emukit.optimize()
 
+# from emukit.core.initial_designs.random_design import RandomDesign
+# desing = RandomDesign(space)
+# X_i = desing.get_samples(10000)
+# Y_i = model_emukit.predict(X_i)
+# print(X_i)
+# print(np.max(Y_i[0]))
+# print(np.min(Y_i[0]))
+# print(np.mean(Y_i[1]))
+
 senstivity = MonteCarloSensitivity(model = model_emukit, input_domain = space)
-main_effects, total_effects, _ = senstivity.compute_effects(num_monte_carlo_points = 10000)
+main_effects, total_effects, m = senstivity.compute_effects(num_monte_carlo_points = 10000)
 print(main_effects)
+print(total_effects)
+print(m)
